2332a1b0d18f3b701d030aa996119e6a3c63ab57
paigejh13 <112772858+paigejh13@users.noreply.github.com>
2023-09-13 22:00:09 -0700

    Delete src directory

--- src/main/java/cpen221practice/ForYouAndMe.java
+++
@@ -1,7 +0,0 @@
-package cpen221practice;
-
-public class ForYouAndMe {
-    public String youandme(String name) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
-}
